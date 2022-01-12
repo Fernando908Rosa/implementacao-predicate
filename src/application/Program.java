7465af3b78a75fb6d37336afc81c1e10c8f75599
fernandorosa <fernandodarosa908@gmail.com>
2022-01-12 14:53:14 -0300

    simplificando predicate

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,7 +3,6 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Predicate;
 
 import entities.Product;
 
@@ -19,11 +18,9 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-	    double min = 100.0;
-	    
-		Predicate<Product> pred = p -> p.getPrice() >= min;
+	    double min = 100.0;	    
 		
-		list.removeIf(pred);
+		list.removeIf(p -> p.getPrice() >= min);
 		
 		for (Product p : list) {
 			System.out.println(p);
